e241ecebe90e6b2d65745c1eb85d1879d7758ae7
Kevin Galligan <galligankevinp@gmail.com>
2019-04-06 17:43:51 +0100

    Fixing up deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,11 @@
 #!/usr/bin/env bash
 
-# Pushes copy of static site to remote master.
-# You should consider testing the changes before running this
-# script, otherwise you might break the site.
+if [[ -z "$1" ]]
+then
+    echo -e "\e[91mWebsite repository path must be passed."
+    exit 1
+fi
 
 ./build.sh
-rm -r /tmp/website-build
-mkdir /tmp/website-build
-mv build/ /tmp/website-build/
-git stash --include-untracked
-
-# We'll use this later to remove annoying files from the commit on the master branch.
-# Exclude build/, the static site files are in there.
-ignorefiles=$(cat .gitignore | sed ':a;N;$!ba;s/\n/ /g')
-
-git checkout master
-git rm -r *
-mv /tmp/website-build/* .
-git add .
-
-# Files in .gitignore are brought over to the master branch. We don't want to keep
-# a copy of .gitignore on the master branch, since it's only supposed to store static
-# files for the website. So here we remove them all the junk before committing.
-for file in "${ignorefiles}"; do git reset HEAD ${file}; done
-
-git commit -m "$(date)"
-git push
-git checkout dev
-git stash apply
+cp -r ./build/* "$1"
+(cd "$1"; git add .; git commit -m "$(date)"; git diff HEAD^ HEAD; git push)
--- pages/hello-world.md
+++ pages/hello-world.md
@@ -2,3 +2,5 @@ title: Fuck You
 date: 2019-04-04
 
 **Hello World**, from a *page*!
+
+Adding another line for no reason other than to test.
