0b3965675b92d5c30dae941f7d4fb4a0d494122b
Kevin Galligan <galligankevinp@gmail.com>
2024-11-21 12:14:17 +0000

    Warning to build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 source env/bin/activate
+if [[ "${@:1}" = "" ]]; then
+	echo "WARNING: You forgot to provide arguments, this will just start the server."
+fi
 python3 app.py "${@:1}" && cp -r ./straight-copy-files/* ./build/
