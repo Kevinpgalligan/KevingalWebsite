9f232fdc32b1fcf569d6d4f2948dadf1638d1f9a
Kevin Galligan <kevin.galligan@manakau.com>
2025-10-26 13:28:28 +0000

    Error handling in build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 source env/bin/activate
 if [[ "${@:1}" = "" ]]; then
-	echo "WARNING: You forgot to provide arguments, this will just start the server."
+	echo "WARNING: You forgot to provide arguments, this won't do anything."
+    exit 1
 fi
 python3 app.py "${@:1}" && cp -r ./straight-copy-files/* ./build/
